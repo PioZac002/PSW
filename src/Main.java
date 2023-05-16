fc2224334a60eed127782863575a371a81fb0842
PioZac002 <piozac002@utp.edu.pl>
2023-05-16 20:56:18 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//
+////
 //        Zadanie1 zadanie1 =new Zadanie1();
 //        zadanie1.Start();
+//
 //        Zadanie2 zadanie2= new Zadanie2();
 //        zadanie2.Start();
+////
 //        Zadanie3 zadanie3=new Zadanie3();
 //        zadanie3.Start();
+//
 //        Zadanie4 zadanie4=new Zadanie4();
 //        zadanie4.Start();
+//
         Zadanie5 zadanie5=new Zadanie5();
         zadanie5.Start();
 
